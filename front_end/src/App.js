b5e59ca34105d1f365b014dd0484e2e962063143
vinay-9 <viinaychopra92.vc@gmail.com>
2021-05-09 13:10:39 +0530

    trial commit

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -7,81 +7,28 @@ import Collapsible from 'react-collapsible';
 import Button from '@material-ui/core/Button'
 import SearchIcon from '@material-ui/icons/Search';
 import Header from './header';
-function App() {
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [items, setItems] = useState([]);
-  const[word,setWord]=useState("");
-
- 
+import ThumbDownOutlinedIcon from '@material-ui/icons/ThumbDownOutlined';
+import ThumbUpAltOutlinedIcon from '@material-ui/icons/ThumbUpAltOutlined';
+import Content from './Content';
+import Login from './login'
+export default function App() {
   useEffect(() => {
       }, [])
-   const url= `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=`+word
-   console.log(url)
-   function Search(){
-    fetch(`https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=`+word, {
-            "method": "GET",
-            "headers": {
-                "x-rapidapi-key": "bd7cf329bemshae8e6053840605ep166442jsnc60488059d95",
-                "x-rapidapi-host": "mashape-community-urban-dictionary.p.rapidapi.com"
-            }
-        })
-        .then(response =>response.json())
-        .then(response=>{
-            console.log(response.list)
-            setItems(response.list)
-        })
-        .catch(err => {
-            console.error(err);
-        });
-   }
-    return (
-      <div>
-          <Header/>
 
-           <Input className="input" id="search" color="black" placeholder="Type the word to search"
-           onKeyDown={e=>{
 
-                if (e. key === 'Enter') {
-                        Search()                    
-                }
-           }}
-           autoFocus="True"
-           onChange={(e) => {
-           e.preventDefault();
-           console.log(e.target.value)
-           setWord(e.target.value)
-        }}>
-            </Input>
+return (
+<Router> 
+    <Switch>
+        <Route path="/app">
+            <Content/>
+         </Route>
 
-        <SearchIcon
-        type="Button"
-        id="search" fontSize="medium" variant="contained" color="black " 
-        onClick={()=>{
-         Search();  
-        }}>
-          Click Here
-        </SearchIcon>
-        
-      <ul className="List">
-       {
-         items.map((product) => {
-          return <li key={product.defid}>
-            <div className="card">
-                <p>{product.definition}</p>
-            <h4> Example </h4>
-            <p className="example">
-            {product.example}
-            </p>            
-            <br></br>
-            
-            </div>  
-          </li>
-           })
-       }
-       </ul>
-      </div>
+        <Route path="/login">
+            <Login/>
+        </Route>
+    </Switch>
+</Router>
+
     );
-  
+
 }
-export default App;
