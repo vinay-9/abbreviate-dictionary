97336eb7e3c922c1e5acb8e728a8e71a6a7441ec
vinay-9 <vinaychopra92.vc@gmail.com>
2022-06-25 15:12:40 +0530

    tmp

--- server.js
+++ server.js
@@ -8,12 +8,20 @@ app.get('/api', (req, res, next) => {
   
   res.send('Node.js Search and Filter');
 });
-app.use(express.static("front_end/build"))
-app.get('*', function (req, res) {
-  res.sendFile(path.join(__dirname, './front_end/build', 'index.html'));
-});
+// app.use(express.static("front_end/build"))
+// app.get('*', function (req, res) {
+//   res.sendFile(path.join(__dirname, './front_end/build', 'index.html'));
+// });
   
-// Start server on PORT 5000
+// // Start server on PORT 5000
+
+
+app.use(express.static(path.resolve(__dirname, "./front_end/build")));
+// Step 2:
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./front_end/build", "index.html"));
+});
+
 let PORT = process.env.PORT || 8000
 app.listen(PORT, () => {
   console.log(`Server started! ${PORT}`);
